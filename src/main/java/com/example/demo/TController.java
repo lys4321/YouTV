cd32ab9de3f0f3d0593d92b6a038abf3a33c7374
unknown <dlfjs4641@naver.com>
2022-09-26 08:34:14 +0900

    lys-2

--- src/main/java/com/example/demo/TController.java
+++ src/main/java/com/example/demo/TController.java
@@ -1,10 +1,19 @@
 package com.example.demo;
 
+import java.io.File;
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONArray;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.DTO.videoDTO;
 import com.example.demo.Mapper.video_mapper;
 
 
@@ -15,7 +24,7 @@ public class TController {
 	
 	@RequestMapping(value="/a", method= {RequestMethod.POST, RequestMethod.GET})
     public String test(){
-		System.out.println(vmap.allLiveList());
-        return "index";
+		System.out.println("a시작");
+        return "modeltest";
 	}
 }
