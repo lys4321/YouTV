714712ae7952a032ac083c92c1c92467725df836
unknown <dlfjs4641@naver.com>
2022-10-04 18:01:26 +0900

    2022.10.04: 사용자 프레임 추출

--- src/main/java/com/example/demo/Controller/VideoController.java
+++ src/main/java/com/example/demo/Controller/VideoController.java
@@ -1,13 +1,35 @@
 package com.example.demo.Controller;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Optional;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.bind.DatatypeConverter;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.support.ResourceRegion;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRange;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.DTO.videoDTO;
 import com.example.demo.Mapper.video_mapper;
@@ -16,6 +38,7 @@ import com.example.demo.Mapper.video_mapper;
 public class VideoController {
 	@Autowired
 	private video_mapper vmap;
+	
 	
 	public void thumbnail() {
 		Runtime run = Runtime.getRuntime();
@@ -47,12 +70,45 @@ public class VideoController {
 		return "Test";
 	}
 	
-	
-	public String thumbnailList(/*web_userDTO user,*/ Model model) {
-		ArrayList<videoDTO> livelist = vmap.liveList();
-		ArrayList<videoDTO> recordlist = vmap.recordList();
+	@RequestMapping(value="/ajax/frame_send", method = {RequestMethod.POST})
+	@ResponseBody
+	public void RTSTest(HttpServletRequest request) {
+		int num = 0;
+		String url = request.getParameter("frame_url");
+		String data = url.split(",")[1];
+		System.out.println(data);
+		byte[] imageByte = DatatypeConverter.parseBase64Binary(data);
+		try {
+			BufferedImage bufimg = ImageIO.read(new ByteArrayInputStream(imageByte));
+			ImageIO.write(bufimg, "png", new File("C:\\YTVrepo\\frame_repo\\"+/*방송코드*/"test.png"));			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 		
-		
-		return "Test :: ";
 	}
+	
+	//YouTV/Video 녹화방송
+		@RequestMapping(value="/YouTV/Video", method= {RequestMethod.GET})
+		public ResponseEntity<ResourceRegion> test3_2(@RequestParam(value="video_code", required=false, defaultValue="not_exist") String video_code, @RequestHeader HttpHeaders headers, Model model) throws UnsupportedEncodingException, IOException 
+		{	videoDTO streaming_video = vmap.selectVideo(video_code); 
+			UrlResource video = new UrlResource("file:"+streaming_video.getSave_url());
+	        ResourceRegion resourceRegion;
+	        final long chunkSize = 1000000L;
+	        long contentLength = video.contentLength();
+	        Optional<HttpRange> optional = headers.getRange().stream().findFirst();
+	        HttpRange httpRange;
+	        if (optional.isPresent()) {
+	            httpRange = optional.get();
+	            long start = httpRange.getRangeStart(contentLength);
+	            long end = httpRange.getRangeEnd(contentLength);
+	            long rangeLength = Long.min(chunkSize, end - start + 1);
+	            resourceRegion = new ResourceRegion(video, start, rangeLength);
+	        } else {
+	            long rangeLength = Long.min(chunkSize, contentLength);
+	            resourceRegion = new ResourceRegion(video, 0, rangeLength);
+	        }
+	        return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+	              .contentType(MediaTypeFactory.getMediaType(video).orElse(MediaType.APPLICATION_OCTET_STREAM))
+	              .body(resourceRegion);
+		}
 }
